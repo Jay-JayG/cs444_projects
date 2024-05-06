b0b3cbd24e786e8db7325c634c3f3827ed853c93
Jay-JayG <jjgochez@gmail.com>
2024-05-05 23:28:41 -0700

    Added simple testing

--- 5_project_file_system/testfs.c
+++ 5_project_file_system/testfs.c
@@ -1,19 +1,45 @@
 #include "image.h"
 #include "block.h"
+#include "ctest.h"
 #include <stdio.h>
+#include <string.h>
+
 #define BLOCK_SIZE 4096
 
-
-int main() {
+void test_bwrite_bread(void)
+{
     unsigned char inbuffer[BLOCK_SIZE] = "Hello";
     unsigned char outbuffer[BLOCK_SIZE];
+    bwrite(0, inbuffer);
+    CTEST_ASSERT(strcmp((char*)bread(0, outbuffer), "Hello") == 0, "Testing writing and reading from disk.\n");
+}
+
+void test_over_bwrite(void)
+{
+    unsigned char inbuffer[BLOCK_SIZE] = "Hello";
+    unsigned char inbuffer2[BLOCK_SIZE] = "Goodbye";
+    unsigned char outbuffer[BLOCK_SIZE];
+    unsigned char outbuffer2[BLOCK_SIZE];
+    bwrite(0, inbuffer);
+    CTEST_ASSERT(strcmp((char*)bread(0, outbuffer), "Hello") == 0, "Testing initial writing and reading from disk.\n");
+
+    bwrite(0, inbuffer2);
+    CTEST_ASSERT(strcmp((char*)bread(0, outbuffer2), "Goodbye") == 0, "Testing if overwritten text is read correctly.\n");
+}
+
+int main()
+{
+    CTEST_VERBOSE(1);
     char *file = "Hello.txt";
     int trunc = 1;
     image_open(file, trunc);
-    bwrite(0, inbuffer);
-    printf("Result %s\n", bread(0, outbuffer));
-    bwrite(3, inbuffer);
-    printf("Result %s\n", bread(3, outbuffer));
+
+    test_bwrite_bread();
+    test_over_bwrite();
+
+    CTEST_RESULTS();
+
+    CTEST_EXIT();
     image_close();
-    return(0);
+    return (0);
 }
